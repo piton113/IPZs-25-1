7de0b5c1f7938263978b126914f4232521b032f3
piton113 <yaromyr.o.zelisko@ukd.edu.ua>
2025-11-25 10:51:56 +0200

    Create minmax.js

---
+++ minmax.js
@@ -0,0 +1,8 @@
+const numbers = [12, 4, 56, -3, 9, 0];
+
+
+const min = Math.min(...numbers);
+const max = Math.max(...numbers);
+
+console.log(`Мінімум: ${min}`); 
+console.log(`Максимум: ${max}`); 
